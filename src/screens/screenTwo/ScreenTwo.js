716323410f8532ac693fdd6da24c4909060b557e
GodFather93 <suryapalsingh.inda@gmail.com>
2020-11-25 10:26:20 +0530

    FixedName break

--- src/screens/screenTwo/ScreenTwo.js
+++ src/screens/screenTwo/ScreenTwo.js
@@ -133,7 +133,7 @@ const styles = StyleSheet.create ({
   },
   name:{
     fontSize:Value(16),
-    fontWeight:'700',
+    fontWeight:'400',
     letterSpacing:0.4,
     color:'grey',
     paddingTop:Value(6)
